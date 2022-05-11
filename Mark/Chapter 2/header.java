47b43327688d6bd8a837c87323c655a20e1c70b9
jholdnak <jholdnak@mailbox.org>
2022-05-10 17:58:48 -0700

    Started working on chapter 2

---
+++ Mark/Chapter 2/header.java
@@ -0,0 +1,16 @@
+public class header {
+    public static void characterJumping(){
+        static int jump;
+        if (jump == 0){
+            System.out.println("");
+            System.out.println("웃");
+            jump++;
+        }
+        else{
+            System.out.println("웃");
+            jump--;
+        }
+        // sleep(1);
+        // system("clear");
+    }
+}
---
+++ Mark/Chapter 2/main.java
@@ -0,0 +1,8 @@
+public class Main{
+    static int myMethod(int size){
+        return size*2;
+    }
+
+    public static void main(String[] args){
+    }
+}
